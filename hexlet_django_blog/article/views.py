173e36c98cf0ccba6a2c5731e29c78785a4717ba
Konstantin Chupin <chupin.k@yandex.ru>
2023-02-06 17:36:54 +0500

    fix views.py from app article

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from hexlet_django_blog.article.apps import ArticleConfig
 
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'article/index.html', context={
+        'app_name': ArticleConfig.name,
+        })  # Указываем путь до шаблона в созданном приложении(оно уже видит дирректорию templates, поэтому в пути указываем дирректорию article(которая находится внутри templates) и шаблон html. Также указываем имя приложения ссылаясь на name расположенное в apps.py(/article)
