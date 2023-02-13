41ed600caa44d01941a1ee90af1108a17af96c09
Konstantin Chupin <chupin.k@yandex.ru>
2023-02-13 19:59:52 +0500

    fix views.py

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+    
+    template_name = "index.html"
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
 
 
 def about(request):
