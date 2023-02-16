b2ee0d1baeb2239532fd63fa93e23c2accf9696c
Konstantin Chupin <chupin.k@yandex.ru>
2023-02-16 20:24:28 +0500

    fix urls.py

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 
-from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentsView, ArticleFormCreateView
 
 
 urlpatterns = [
-        path('', views.index),
+        path('', IndexView.as_view(), name='articles'),
+        path('<int:id>/', ArticleView.as_view(), name='article'),
+        path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
+        path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
